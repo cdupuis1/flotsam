5cd2daa3f246f1a6137035b5a53561299441fbda
Chad Dupuis <cdupuis1@gmail.com>
2019-09-17 21:28:38 -0400

    Add TM4C123 project to turn the LED different colors.

---
+++ bare_metal/TM4C123/rgb_switch/Debug/tm4c1230h6pm_startup_ccs.d
@@ -0,0 +1,21 @@
+# FIXED
+
+tm4c1230h6pm_startup_ccs.obj: ../tm4c1230h6pm_startup_ccs.c
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdint.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_stdint40.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/stdint.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/cdefs.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_types.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_types.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_stdint.h
+tm4c1230h6pm_startup_ccs.obj: /Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_stdint.h
+
+../tm4c1230h6pm_startup_ccs.c: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdint.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_stdint40.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/stdint.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/cdefs.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_types.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_types.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_stdint.h: 
+/Applications/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_stdint.h: 
