10f36ca8c776d8ff11a65ffe030285e68f08953b
Chad Dupuis <cdupuis1@gmail.com>
2019-10-31 21:43:40 -0400

    Add numpy test drive script.

---
+++ python/numpy_tryme.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+# numpy_tryme.py
+#
+# Just taking numpy for a test drive...
+
+import numpy as np
+
+# Declare numpy array
+x = np.array([1, 2, 3])
+print(x)
+
+# Declare a numpy matrix
+y = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(y)
+
+# Declare 3x3 matrix of all ones
+z = np.ones((3, 3))
+print(z)
+
